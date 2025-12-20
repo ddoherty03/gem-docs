a456f13e0a4c84e4070d0702161bdb299cad317c
Daniel E. Doherty <ded@ddoherty.net>
2025-12-20 05:08:26 -0600

    Put emacs methods in a module like the others.

--- lib/gem_docs/emacs.rb
+++ lib/gem_docs/emacs.rb
@@ -1,52 +1,55 @@
 # frozen_string_literal: true
 
 module GemDocs
-  extend self
+  module Emacs
+    # Run all org-babel code blocks
+    def self.tangle
+      ensure_saved
+      expr = <<~ELISP
+        (progn
+          (find-file "#{ORG}")
+          (require 'ob-ruby)
+          (org-babel-execute-buffer)
+          (save-buffer)
+          "OK")
+      ELISP
 
-  # Save README.org buffer if modified inside a running emacsclient
-  def ensure_saved
-    expr = <<~ELISP
-      (progn
-        (when (get-buffer "#{ORG}")
-          (with-current-buffer "#{ORG}"
-            (when (buffer-modified-p)
-              (save-buffer))))
-        "")
-    ELISP
-
-    unless system("emacsclient", "--quiet", "--eval", expr)
-      abort("Failed to ensure README.org was saved")
+      if system("emacsclient", "--quiet", "--eval", expr)
+        FileUtils.touch(STAMP)
+      else
+        puts "ERROR"
+        abort "Babel execution failed"
+      end
     end
-  end
 
-  # Run all org-babel Ruby blocks
-  def evaluate_examples
-    expr = <<~ELISP
-      (progn
-        (find-file "#{ORG}")
-        (require 'ob-ruby)
-        (org-babel-execute-buffer)
-        (save-buffer)
-        "OK")
-    ELISP
-
-    if system("emacsclient", "--quiet", "--eval", expr)
-      FileUtils.touch(STAMP)
-    else
-      puts "ERROR"
-      abort "Babel execution failed"
+    # Export README.org → README.md via ox-gfm
+    def self.export_readme
+      expr = <<~ELISP
+        (progn
+          (find-file "#{ORG}")
+          (require 'ox-gfm)
+          (org-gfm-export-to-markdown))
+      ELISP
+      system("emacsclient", "--eval", expr)
     end
-  end
 
-  # Export README.org → README.md via ox-gfm
-  def export_readme
-    expr = <<~ELISP
-      (progn
-        (find-file "#{ORG}")
-        (require 'ox-gfm)
-        (org-gfm-export-to-markdown))
-    ELISP
+    class << self
+      private
 
-    system("emacsclient", "--eval", expr)
+      # Save README.org buffer if modified inside a running emacsclient
+      def ensure_saved
+        expr = <<~ELISP
+          (progn
+            (when (get-buffer "#{ORG}")
+              (with-current-buffer "#{ORG}"
+                (when (buffer-modified-p)
+                  (save-buffer))))
+            "")
+        ELISP
+        unless system("emacsclient", "--quiet", "--eval", expr)
+          abort("Failed to ensure README.org was saved")
+        end
+      end
+    end
   end
 end
